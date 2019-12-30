fdf23b6439fa7f30caacb714795969da0367d170
VIVAN ABRAHAM <vivanabraham@VIVANs-MacBook-Pro.local>
2019-12-30 10:44:04 -0600

    Adding logs

--- src/main/java/com/ibm/health/HealthEndpoint.java
+++ src/main/java/com/ibm/health/HealthEndpoint.java
@@ -1,5 +1,7 @@
 package com.ibm.health;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class HealthEndpoint implements HealthIndicator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthEndpoint.class);
+
     @Override
     @GetMapping("/health")
     public Health health() {
@@ -17,6 +21,9 @@ public class HealthEndpoint implements HealthIndicator {
            return Health.down().withDetail("Not healthy", 500).build();
           }
         */
+
+        LOGGER.info("App is healthy!!");
+
         return Health.up().build();
     }
 }
